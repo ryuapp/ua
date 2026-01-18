cff7e0bd76285ddc090bb42c3f58323b7e35614b
ryu <114303361+ryuapp@users.noreply.github.com>
2026-01-19 00:24:55 +0900

    View User Agent in details

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import { parse } from "bowser";
+
 export default {
   fetch(request) {
     const url = new URL(request.url);
     if (url.pathname === "/") {
       const userAgent = request.headers.get("User-Agent");
       if (!userAgent) return new Response("No User-Agent", { status: 400 });
-      return new Response(userAgent, { status: 200 });
+
+      const result = parse(userAgent);
+
+      return Response.json(result);
     }
 
     return new Response("Not found", { status: 404 });
